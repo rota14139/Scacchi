e65c9c29e5165816386b2bfcf69a40e8a5fc5b6e
rota14139 <rota.14139@itispaleocapa.it>
2018-12-14 00:59:17 +0100

    [14-05-18] Aggiunti due nuovi errori, in server.cpp sviluppata parte comunicazione verso client, aggiunti movimenti speciali in scacchiera .h(arrocco , re muove) e aggiunto controllo del turno/colore

--- scacchiera.h
+++ scacchiera.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "pezzi.h"
+#include <typeinfo>
 
 using namespace std;
 
@@ -32,8 +33,8 @@ public:
             for(int i =0;i<8;i++)
                 casella[j][i]= NULL;
 
-//        for(int i =0;i<8;i++)//pedoni bianchi
-//            casella[6][i]= new pedone(0);
+        for(int i =0;i<8;i++)//pedoni bianchi
+            casella[6][i]= new pedone(0);
         casella[7][0]= new torre(0);
         casella[7][1]= new cavallo(0);
         casella[7][2]= new alfiere(0);
@@ -43,9 +44,9 @@ public:
         casella[7][6]= new cavallo(0);
         casella[7][7]= new torre(0);
     }
-    int sposta(char mov[5]);
-    int controlla_movimento(char mov[5]); //controlla se un movimento si possa fare
-    bool scacco(char cella[3]); //controlla se una deter        minata cella è sotto scacco (utile per re e per arrocco)
+    int sposta(char mov[5],bool turno);
+    int controlla_movimento(char mov[5]),bool turno; //controlla se un movimento si possa fare
+    bool scacco(char cella[3],bool colore); //controlla se una deter        minata cella è sotto scacco (utile per re e per arrocco)
     bool controllo_tragitto(char mov[5]); // bool controllo_caselle_in mezzo(char mov[5]);
     void movimento(char mov[5]); //muove un pezzo (senza controllo... quindi farlo prima)
     char * getRe(int i);
@@ -72,24 +73,42 @@ void scacchiera::stampa_scacchiera()
             cout<<" "<<8-i;
             if(i==7)
                 cout<<"\n   -------------------------------\n   A   B   C   D   E   F   G   H";
-            cout<<endl<<" |"<<endl;
+            else
+                cout<<endl<<" |"<<endl;
         }
 }
 char * scacchiera::getRe(int i)
 {
     return pos_re[i];
 }
-int scacchiera::sposta(char mov[5])
+int scacchiera::sposta(char mov[5],bool turno)
 {
-    int err=controlla_movimento(mov);
-    switch(err)
+    int err=controlla_movimento(mov,turno);
+    switch(err)//0 = normale >=10 mosse speciali
     {
-    case -1: //re muove
+    case 0:
+        {
+            pezzo *mangiato = casella[8-(mov[3]-'0')][(mov[2]-'A')];
+            movimento(mov);
+            if (scacco(pos_re[casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore()],casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore()))
+            {
+                char sup[5]={mov[2],mov[3],mov[0],mov[1],0};
+                movimento(sup);
+                casella[8-(mov[3]-'0')][(mov[2]-'A')]=mangiato;
+                return 8;
+            }
+            if(typeid(*casella[8-(mov[3]-'0')][(mov[2]-'A')])==typeid(pedone))//se è un pedone
+                if((mov[3])== (casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore() ? '1' : '8'))
+                    cout<<"askjfdvgi";
+
+            return 0;
+        }
+    case 10: //re muove
         {
             char pos[3]={mov[2],mov[3],0};
             pezzo *mangiato = casella[8-(pos[1]-'0')][(pos[0]-'A')];
             movimento(mov);
-            if(scacco(pos))
+            if(scacco(pos,casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore()))
             {
                 char sup[5]={mov[2],mov[3],mov[0],mov[1],0};
                 movimento(sup);
@@ -99,16 +118,48 @@ int scacchiera::sposta(char mov[5])
             strcpy(pos_re[casella[8-(pos[1]-'0')][(pos[0]-'A')]->get_colore()],pos);
             return 0;
         }
-    case 0:
+    case 11: //arrocco
         {
-            movimento(mov);
-            return 0;
+            int colore= casella[8-(mov[1]-'0')][mov[0]-'A']->get_colore();
+            if(casella[8-(mov[1]-'0')][mov[0]=='C' ? 0 : 7]!=NULL)
+                if(typeid(*casella[8-(mov[1]-'0')][mov[0]=='C' ? 0 : 7])==typeid(torre))//se è una torre
+                    if(!casella[8-(mov[1]-'0')][mov[0]=='C' ? 0 : 7]->getMosso()); //se la torre non si è ancora mossa
+                    {
+                        char mov_torre[5]={mov[2]=='C' ? 'A' : 'H',mov[1],mov[2]=='C' ? 'D':'F',mov[1],0};
+                        if(controllo_tragitto(mov_torre))
+                        {
+                            char sup[5];
+                            strcpy(sup,mov);
+
+                            if(sup[0]>sup[2])
+                                sup [2]--;
+                            else if(sup[0]<sup[2])
+                                sup[2]++;
+                            while(sup[0]!=sup[2])
+                            {
+                                char ctrl_scacco[3]={sup[0],sup[1],0};
+                                if(casella[8-(ctrl_scacco[1]-'0')][ctrl_scacco[0]-'A']!=NULL)
+                                    if(scacco(ctrl_scacco,colore))
+                                        return 6;
+                                if(sup[0]<sup[2])
+                                    sup [0]++;
+                                else if(sup[0]>sup[2])
+                                    sup[0]--;
+                            }
+                            movimento(mov);
+                            movimento(mov_torre);
+                            char new_pos_re[3]={mov[3],mov[2],0};
+                            strcpy(pos_re[casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore()],new_pos_re);
+                            return 0;
+                        }
+                    }
+            return 6;
         }
     default:
         return err;
     }
 }
-bool scacchiera::scacco(char cella[3])
+bool scacchiera::scacco(char cella[3],bool colore)
 {
 
     char mov[5];
@@ -121,18 +172,15 @@ bool scacchiera::scacco(char cella[3])
             mov[0]=i+'A';
             mov[1]=(7-j)+'1';
             if(casella[j][i]!=NULL)
-                if(casella[8-(mov[1]-'0')][(mov[0]-'A')]->get_colore()!=casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore())
+                if(casella[8-(mov[1]-'0')][(mov[0]-'A')]->get_colore()!=colore)
                     if((abs(mov[1]-mov[3])==0 || abs(mov[0]-mov[2])==0||abs(mov[1]-mov[3])==abs(mov[0]-mov[2]))||((abs(mov[0]-mov[2])==1&&abs(mov[1]-mov[3])==2)||(abs(mov[0]-mov[2])==2&&abs(mov[1]-mov[3])==1)))
                     {
-
-
                         switch( casella[8-(mov[1]-'0')][(mov[0]-'A')]->movimento(mov))//controllo se il movimento del pezzo è legale
                             {
                             case 1: //normale
-                            {
                                 if(controllo_tragitto(mov))//controllo caselle_in mezzo
                                     return true;
-                            }
+                                break;
                             case 3: //pedone mangia
                             case 4://cavallo che non deve controllare se ci sono pezzi in mezzo
                             case 5://re muove
@@ -142,7 +190,7 @@ bool scacchiera::scacco(char cella[3])
         }
     return false;
 }
-int scacchiera::controlla_movimento(char mov[5])
+int scacchiera::controlla_movimento(char mov[5],bool turno)
 {
     if(strlen(mov)==4)
     {
@@ -151,7 +199,7 @@ int scacchiera::controlla_movimento(char mov[5])
             if(mov[1]!=mov[3] || mov[0]!=mov[2])// se le celle sono uguali
             {
                 if(casella[8-(mov[1]-'0')][(mov[0]-'A')]!=NULL) //se la casella di partenza non è vuota
-                {
+                {//mettere controllo se è un tuo pezzo
                     if(casella[8-(mov[3]-'0')][(mov[2]-'A')]!=NULL)   //se la casella di arrivo non è vuota
                     {
                         if(casella[8-(mov[1]-'0')][(mov[0]-'A')]->get_colore()!=casella[8-(mov[3]-'0')][(mov[2]-'A')]->get_colore())//se nella cella di arrivo non c'è un pezzo dello stesso colore
@@ -159,13 +207,15 @@ int scacchiera::controlla_movimento(char mov[5])
                             {
                             case 1: //normale
                                 return controllo_tragitto(mov)? 0 : 6;//controllo caselle_in mezzo;
+                            case 0://mossa sbagliata
+                            case 6://arrocco
                             case 2://perdone muove avanti
                                 return 6;  //false perche' non puo' muoversi avanti visto che c'e' un pezzo nemico
                             case 3: //pedone mangia return true visto che nella cella nemica c'è per forza un pezzo nemico
                             case 4://cavallo che non deve controllare se ci sono pezzi in mezzo
                                 return 0;
                             case 5://re muove
-                                return -1;
+                                return 10;
                             }
                             return 5;//alleato nella cella di arrivo
                     }
@@ -175,12 +225,15 @@ int scacchiera::controlla_movimento(char mov[5])
                             case 1: //normale
                             case 2://perdone muove avanti
                                 return controllo_tragitto(mov)? 0 : 6;    //funzione perchè la casella davanti e' vuota ma se si muove di 2 potrebbe esserci un pezzo in mezzo
+                            case 0://mossa sbagliata
                             case 3: //mangia pedone
                                 return 6; //nella casella dove potrebbe mangiare non c'è nessun pezzo nemico
                             case 4://cavallo che non deve controllare se ci sono pezzi in mezzo
                                 return 0;
                             case 5://re muove
-                                return -1;
+                                return 10;
+                            case 6://arrocco
+                                return controllo_tragitto(mov)? 11 : 6;
                             }
                 }//casella di partenza  vuota
                 return 4;
@@ -211,7 +264,6 @@ bool scacchiera::controllo_tragitto(char mov[5])
         sup[3]--;
     while(sup[0]!=sup[2]||sup[1]!=sup[3])
     {
-        cout<<sup<<" ";
         if(sup[0]<sup[2])
             sup [0]++;
         else if(sup[0]>sup[2])
--- server.cpp
+++ server.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
 #include <stdlib.h>
+#include <iostream>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "scacchiera.h"
+#include "pezzi.h"
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -12,86 +17,262 @@
 #include <arpa/inet.h>
 
 using namespace std;
+/*
+classe scacchiera ->collezione di->
+astract classe pezzo ->sottoclassi->
+    -re
+    -regina
+    -torre
+    -alfiere
+    -cavallo
+    -pedone
+*/
 
-char scacchiera[8][8]={{'t','c','a','r','k','a','c','t'},{'p','p','p','p','p','p','p','p'},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0},{'P','P','P','P','P','P','P','P'},{'T','C','A','R','K','A','C','T'}};
+void login (int player1,int player2, char player_name[2][16]);
+void toup(char stringa[]);
+void comunica_movimento(char mov[5],int player1,int player2);
+char richiedi_promozione(int player);
+void comunica_promozione(char* pos ,char pezzo,int player1,int player2);
+void comunica_errore(int err,int player);
+void comunica_fine(bool turno,int player1,int player2,char * player_name);
+scacchiera scacchiera1;
 
-bool scacco(char movimento[]); //controlla scacco
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    //collegamento
-    int sockfd, newsockfd, portno, clilen,n;
+    int sockfd, player1,player2, portno, clilen,n;
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in cli_addr;
 	if (argc < 2)
 	{
 		cout<<"Indirizzo IP richiesto\n";
-		return 0;
+		return -1;
 	}
 	if (argc < 3)
 	{
-		cout<<"Numro porta richiesto\n";
-		return 0;
+		cout<<"Numero porta richiesto\n";
+		return -1;
 	}
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
+    {
 		cout<<"errore di apertura del socket\n";
+        return -1;
+    }
 	memset(&serv_addr,0,sizeof(serv_addr));
 	portno = atoi(argv[2]);
 	serv_addr.sin_family = AF_INET;
 	inet_aton(argv[1],&(serv_addr.sin_addr));
 	serv_addr.sin_port = htons(portno);
-
 	if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
 		cout<<"errore di collegamento\n";
-
-	listen(sockfd,5);
+        return -1;
+    }
+	listen(sockfd,2);
 	clilen = sizeof(cli_addr);
 
-	newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,(socklen_t *)&clilen);
-	if (newsockfd == 0)
-		cout<<"errore su accept()\n";
-	memset(buffer,0,50);
 
-    //comunicazione
+	cout<<"-server- attessa player1\n";
+	player1 = accept(sockfd,(struct sockaddr *) &cli_addr,(socklen_t *)&clilen);
+	if (player1 == 0)
+    {
+		cout<<"errore su accept() player1\n";
+        return -1;
+    }
+	cout<<"-server- player1 connesso correttamente\n-server- attessa player2\n";
+	player2 = accept(sockfd,(struct sockaddr *) &cli_addr,(socklen_t *)&clilen);
+    if (player2 == 0)
+    {
+		cout<<"errore su accept() player2\n";
+        return -1;
+    }
+	cout<<"-server- player2 connesso correttamente\n";
+    char buffer[20];
+    char player_name[2][16];
+    bool turno=false; //true = nero, false = bianco
+	memset(buffer,0,20);
+
+	login(player1,player2,player_name);
+
     do{
+        if(read((turno ? player2 : player1),buffer,sizeof(buffer))<0)
+           cout<<"client -> server: errore read player"<<(turno ? 2 : 1)<<endl;
+        else
+            cout<<"client -> server: lettura mossa player"<<(turno ? 2 : 1)<<" buffer letto: "<<buffer<<endl;
+        cout<<"a";
+        toup(buffer);
+        cout<<"b";
+        switch(buffer[0])
+        {
+            case 'M':
+            {
 
+                int err=scacchiera1.sposta(&buffer[1],turno);
+                switch(err)
+                {
+                    case 0:
+                    {
+                        comunica_movimento(&buffer[1],player1,player2);
+                        turno=!turno;
+                    }break;
+                    case 12:
+                    {
+                        char  pezzo=richiedi_promozione(turno? player2:player1);
+                        scacchiera1.promozione(&buffer[2],pezzo);
+                        comunica_promozione(&buffer[1],pezzo,player1,player2);
+                        turno=!turno;
+                    }break;
+//                    case 0:
+//                    {
+//                        comunica_movimento(&buffer[1],player1,player2);
+//                        turno=!turno;
+//                    }break;
+                    default:
+                        comunica_errore(err,turno ? player2 : player1);
 
-    }while();
+                }
+            }break;
+            case 'R':
+            {
+                comunica_fine(turno,player1,player2,player_name[turno]);
+            }break;
+            default:
+                    comunica_errore(1,turno ? player2 : player1);
+        }
+    }while(true);
+
 }
 
-bool scacco()
+void login (int player1,int player2,char player_name[2][16])
+{
+    char buffer[20];
+	memset(buffer,0,20);
+
+    if(write(player1,"NB",3)<0)
+       cout<<"server -> client: errore write player1\n";
+    else
+        cout<<"server -> client: richiesta player_name1\n";
+
+    if(read(player1,buffer,sizeof(buffer))<0)
+       cout<<"client -> server: errore read player1\n";
+    else
+        cout<<"client -> server: lettura player_name1: "<<&buffer[1]<<endl;
+    strcpy(player_name[0],&buffer[1]);
+
+    if(write(player2,"NN",3)<0)
+       cout<<"server -> client: errore write player2\n";
+    else
+        cout<<"server -> client: richiesta player_name2\n";
+
+    if(read(player2,buffer,sizeof(buffer))<0)
+       cout<<"client -> server: errore read player2\n";
+    else
+        cout<<"client -> server: lettura player_name2: "<<&buffer[1]<<endl;
+    strcpy(player_name[1],&buffer[1]);
+
+    strcpy(buffer,"I");
+    strcat(buffer,player_name[1]);
+    if(write(player1,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player1\n";
+    else
+        cout<<"server -> client: comunicazione player_name2 a player1\n";
+
+    strcpy(buffer,"I");
+    strcat(buffer,player_name[0]);
+    if(write(player2,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player2\n";
+    else
+        cout<<"server -> client: comunicazione player_name1 a player2\n";
+    cout<<"\n\n";
+}
+void toup(char stringa[])
+{
+    int i =-1;
+    while(stringa[(++i)]!=0)
+        stringa[i]=toupper(stringa[i]);
+}
+
+void comunica_movimento(char mov[5],int player1,int player2)
+{
+    char buffer[20];
+    strcpy(buffer,"M");
+    strcat(buffer,mov);
+    if(write(player1,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player1\n";
+    else
+        cout<<"server -> client: comunicazione movimento a player1\n";
+
+    if(write(player2,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player2\n";
+    else
+        cout<<"server -> client: comunicazione movimento a player2\n";
+
+}
+
+void comunica_errore(int err,int player)
 {
 
+    char buffer[20];
+    buffer[0]='E';
+    buffer[1]=err+'0';
+    buffer[2]=0;
+    if(write(player,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player\n";
+    else
+        cout<<"server -> client: comunicazione errore a player\n";
 
 }
- /*
- switch(scacchiera[i][j])
+
+void comunica_fine (bool turno,int player1,int player2,char * player_name)
 {
-    case 'P':
-        cout<<"♟";break;
-    case 'C':
-        cout<<"♞";break;
-    case 'A':
-        cout<<"♝";break;
-    case 'T':
-        cout<<"♜";break;
-    case 'K':
-        cout<<"♚";break;
-    case 'R':
-        cout<<"♛";break;
-    case 'p':
-        cout<<"♙";break;
-    case 'c':
-        cout<<"♘";break;
-    case 'a':
-        cout<<"♗";break;
-    case 't':
-        cout<<"♖";break;
-    case 'k':
-        cout<<"♔";break;
-    case 'r':
-        cout<<"♕";break;
+    char buffer[20];
+    strcpy(buffer,"F");
+    strcat(buffer,turno ? "B":"N");
+    strcat(buffer,player_name);
+    cout<<"-server- vincitore: "<<player_name<<", player: "<<(turno? "bianco" : "nero")<<endl;
+    if(write(player1,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player1\n";
+    else
+        cout<<"server -> client: comunicazione fine partita a player1\n";
+
+    if(write(player2,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player2\n";
+    else
+        cout<<"server -> client: comunicazione fine partita a player2\n";
 }
-*/
+
+char richiedi_promozione(int player)
+{
+    char buffer[20];
+    strcpy(buffer,"P?");
+    if(write(player,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player\n";
+    else
+        cout<<"server -> client: richiesta promozione\n";
+
+
+    if(read(player,buffer,sizeof(buffer))<0)
+       cout<<"client -> server: errore read player\n";
+    else
+        cout<<"client -> server: ricevo pezzo promozione\n";
+    return buffer[1];
+}
+
+void comunica_promozione(char* movimento, char pezzo,int player1,int player2)
+{
+    char buffer[20];
+    strcpy(buffer,"P");
+    strcat(buffer,&pezzo);
+    strcat(buffer, movimento);
+    if(write(player1,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player1\n";
+    else
+        cout<<"server -> client: comunicazione promozionea player1\n";
+
+    if(write(player2,buffer,strlen(buffer)+1)<0)
+       cout<<"server -> client: errore write player2\n";
+    else
+        cout<<"server -> client: comunicazione fine partita a player2\n";
+
+}
